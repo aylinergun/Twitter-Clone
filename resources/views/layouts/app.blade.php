16d5072cc74a9e81d5d800f7044592c0f3080394
aylinergun <aylinergun@yahoo.com>
2021-06-23 16:29:39 +0300

    template fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,83 +1,59 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width , initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Twitter Clone</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/GrayGrids/LineIcons/LineIcons.css">
+    <link rel="stylesheet" href="assets/css/stylesheets/screen.css">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/lib/LineIcons.min.css">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+<body class="bg-white">
 
-                    </ul>
+    <!-- wrapper start -->
+    <div class="">
+        <div class="container mx-auto">
+            <div class="flex md:flex-no-wrap flex-wrap">
+                <div class="md:w-1/2 w-full bg-white">
+                    <div class="inner pr-12 px-4 py-1" id="leftNavbar">
+                        <!-- logo start -->
+                        <div class="logo twitter-color w-12 transition-3 twitter-light-blue-on-hover p-2 ml-2 cursor-pointer rounded-full">
+                            <svg viewBox="0 0 24 24"><g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g></svg>
+                        </div>
+                        <!-- logo end -->
+                          @include('layouts.left-side-navigation')
+                    </div>
+                </div>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                <div class="w-full py-0 border border-t-0 border-b-0 ">
+                   @include('layouts.home')
+                </div>
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                <div class="md:w-2/3 w-full px-4 py-0">
+                    <div class="inner right-column" id="rightSide">
+                      @include('layouts.right-side-navigation')
+                    </div>
                 </div>
             </div>
-        </nav>
+        </div>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
     </div>
+    <!-- wrapper end-->
+
+    <script src="assets/js/lib/jquery.min.js"></script>
+    <script src="assets/js/lib/jquery.sticky.js"></script>
+    <script src="assets/js/script.js"></script>
+
+    <main class="py-4">
+         @yield('content')
+    </main>
+
+
 </body>
+
 </html>
--- resources/views/layouts/right-side-navigation.blade.php
+++ resources/views/layouts/right-side-navigation.blade.php
@@ -1,3 +1,18 @@
+<div class="block search-block my-2">
+
+    <!-- form start -->
+    <form class="w-full max-w-sm">
+        <div class="flex items-center py-2 bg-gray-300 rounded-full search-input-wrappper">
+            <span class="w-8 px-1 mx-2">
+                <svg viewBox="0 0 24 24" class="r-1re7ezh r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-18qmn74 r-1hfyk0a r-bnwqim r-1plcrui r-lrvibr"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+            </span>
+            <input class="appearance-none search-input bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight font-normal focus:outline-none" type="text" placeholder="Search Twitter">
+        </div>
+    </form>
+    <!-- form end -->
+
+</div>
+
 <div class="block trend-block my-4 rounded-lg">
 
     <!-- trend block header start -->
@@ -102,4 +117,103 @@
         <!-- trend list end -->
     </div>
     <!-- trend list wrapper end -->
+</div>
+
+<div class="block who-to-follow-block twitter-extra-light-gray-bg my-5 rounded-bl-lg">
+
+    <!-- who to follow block header start -->
+    <div class="who-to-follow-block-header clearfix px-3 py-3">
+        <span class="float-left text-xl font-extrabold text-gray-900">Who to follow</span>
+    </div>
+    <!-- who to follow block header end -->
+
+    <hr class="h-px bg-gray-300">
+
+    <!-- who to follow list wrapper start -->
+    <div class="who-to-flow-list-wrapper">
+        <!-- who to follow list start -->
+        <ul class="who-to-follow-list">
+
+
+            <!-- who to follow list item start -->
+            <li class="who-to-follow-list-item">
+                <!-- who to follow list link start -->
+                <a href="" class="who-to-follow-link block flex">
+                    <span class="w-auto p-2">
+                        <img class="w-24 block rounded-full shadow-2xl" src="https://pbs.twimg.com/profile_images/862681541620645892/6Bn_15Jz_normal.jpg" alt="Profile Image">
+                    </span>
+                    <span class="leading-none w-full p-2 flex items-center">
+                       <span>
+                            <b class="hover:underline text-gray-900">NoorUllah Mengal</b>
+                            <span class="mt-1 text-sm text-gray-600">@SecInfoPAT</span>
+                    </span>
+                    </span>
+                    <span class="w-auto text-right flex items-center p-2 pr-4 justify-center">
+                        <button class="hover:bg-blue-500 transition-3 hover:text-white twitter-border rounded-full twitter-color px-4 py-px font-bold ">Follow</button>
+                    </span>
+                </a>
+                <!-- who to follow list link end -->
+            </li>
+            <!-- who to follow list item end -->
+
+
+            <!-- who to follow list item start -->
+            <li class="who-to-follow-list-item">
+                <!-- who to follow list link start -->
+                <a href="" class="who-to-follow-link block flex">
+                    <span class="w-auto p-2">
+                        <img class="w-24 block rounded-full shadow-2xl" src="https://pbs.twimg.com/profile_images/1156996248186081281/QSXy_pQy_normal.jpg" alt="Profile Image">
+                    </span>
+                    <span class="leading-none w-full p-2 flex items-center">
+                       <span>
+                            <b class="hover:underline text-gray-900">Armeena Khan</b>
+                            <span class="mt-1 text-sm text-gray-600">@ArmeenaRK</span>
+                    </span>
+                    </span>
+                    <span class="w-auto text-right flex items-center p-2 pr-4 justify-center">
+                        <button class="hover:bg-blue-500 transition-3 hover:text-white twitter-border rounded-full twitter-color px-4 py-px font-bold ">Follow</button>
+                    </span>
+                </a>
+                <!-- who to follow list link end -->
+            </li>
+            <!-- who to follow list item end -->
+
+
+            <!-- who to follow list item start -->
+            <li class="who-to-follow-list-item">
+                <!-- who to follow list link start -->
+                <a href="" class="who-to-follow-link block flex">
+                    <span class="w-auto p-2">
+                        <img class="w-24 block rounded-full shadow-2xl" src="https://pbs.twimg.com/profile_images/1162860983234244610/SkXnT9lp_normal.jpg" alt="Profile Image">
+                    </span>
+                    <span class="leading-none w-full p-2 flex items-center">
+                       <span>
+                            <b class="hover:underline text-gray-900">Raheeq Abbasi</b>
+                            <span class="mt-1 text-sm text-gray-600">@RaheeqAbbasi</span>
+                    </span>
+                    </span>
+                    <span class="w-auto text-right flex items-center p-2 pr-4 justify-center">
+                        <button class="hover:bg-blue-500 transition-3 hover:text-white twitter-border rounded-full twitter-color px-4 py-px font-bold ">Follow</button>
+                    </span>
+                </a>
+                <!-- who to follow list link end -->
+            </li>
+            <!-- who to follow list item end -->
+
+            <!-- who to follow list item start -->
+            <li class="who-to-follow-list-item">
+                <!-- who to follow list link start -->
+                <a href="" class="who-to-follow-link block flex p-3 tracking-wider">
+                    Show more
+                </a>
+                <!-- who to follow list link end -->
+            </li>
+            <!-- who to follow list item end -->
+
+
+        </ul>
+        <!-- who to follow list end -->
+    </div>
+    <!-- who to follow list wrapper end -->
+
 </div>
